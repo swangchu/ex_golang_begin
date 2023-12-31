17cab413b73107c2855de11690ee454b8244a93d
Sonam Wangchu <swangchu79@gmail.com>
2023-12-31 13:28:00 +0800

    Create beginner_4.go

---
+++ beginner_4.go
@@ -0,0 +1,6 @@
+// Write a program which accepts a sequence of comma-separated numbers from console and generate an 
+// slice out of them. Return the slice.
+
+// Suppose the following input is supplied to the program: 34, 67, 55, 33, 12, 98.
+
+// Then, the output should be: [34 67 55 33 12 98]
